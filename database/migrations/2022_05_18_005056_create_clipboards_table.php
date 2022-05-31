f430de7ed9ef451d5ec7955012a809505d746ecc
Adham Miftah <77636588+adhammiftah@users.noreply.github.com>
2022-05-31 12:59:34 +0700

    fix data type issue

--- database/migrations/2022_05_18_005056_create_clipboards_table.php
+++ database/migrations/2022_05_18_005056_create_clipboards_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('clipboards', function (Blueprint $table) {
-            $table->id();
+            $table->string('id');
             $table->text('content');
             $table->timestamps();
         });
